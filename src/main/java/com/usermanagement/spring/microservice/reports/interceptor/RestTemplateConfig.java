83da527d936344673d43765b6e3009acd64f5b21
amitconcepta <amit@conceptaclouds.com>
2025-04-09 16:38:12 +0530

    added interceptor file

---
+++ src/main/java/com/usermanagement/spring/microservice/reports/interceptor/RestTemplateConfig.java
@@ -0,0 +1,13 @@
+package com.usermanagement.spring.microservice.reports.interceptor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
